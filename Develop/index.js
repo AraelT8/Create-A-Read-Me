2241d8ed744984ab9f5db3f6ea837a26e99a33b9
Arael <araeltiejerina@gmial.com>
2023-05-18 10:18:46 -0500

    imported the necessary packages and wrote the functions for taking on the users input plus functions to write the data to a md file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,71 @@
-// TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function readMePrompts() {
+    return inquirer.prompt([
+        {
+            name: 'title',
+            message: 'What is your projects name?',
+            type: 'input'
+        },
+        {   name: 'license',
+            message: 'What type of license does this project use?',
+            type: 'list',
+            choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None']
+        },
+        {
+            name: 'description',
+            message: 'What is your project about?',
+            type: 'input'
+        },
+        {
+            name: 'installation',
+            message: 'How do you install your project?',
+            type: 'input'
+        },
+        {
+            name: 'usage',
+            message: 'How do you use your project?',
+            type: 'input'
+        },
+        {
+            name: 'contributers',
+            message: 'Who contributed to this project?',
+            type: 'input'
+        },
+        {
+            name: 'tests',
+            message: 'How do you test your project?',
+            type: 'input'
+        },
+        {
+            name: 'github',
+            message: 'What is your github username?',
+            type: 'input'
+        },
+        {
+            name: 'email',
+            message: 'What is your email address?',
+            type: 'input'
+        },
+        {
+            name: 'fileName',
+            message: 'What do you wish to name your readme?',
+            type: 'input'
+        },
+])};
 
-// TODO: Create a function to initialize app
-function init() {}
+function writeToFile(fileName, data) {
+    fs.appendFile(`${fileName}.md`, data, (err) => err ? console.error(err) : console.log(`${fileName}.md created!`));
+}
 
-// Function call to initialize app
+
+async function init() {
+    let answers = await readMePrompts();
+    writeToFile(answers.fileName, generateMarkdown(answers));
+}
+
+
 init();
